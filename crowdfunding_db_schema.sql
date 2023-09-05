0289fc9da64bfe2d66e28f218abc1bffcb1ff6da
Jeff North <jnorthdata@gmail.com>
2023-09-05 11:04:07 -0700

    Project completed

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,64 @@
+-- Create the database table schema
+
+CREATE SCHEMA crowdfunding;
+
+
+CREATE TABLE crowdfunding.campaign (
+    cf_id integer NOT NULL,
+    contact_id integer,
+    company_name character varying(60),
+    description character varying(100),
+    goal double precision,
+    pledged double precision,
+    outcome character varying(20),
+    backers_count integer,
+    country character(2),
+    currency character(3),
+    launch_date date,
+    end_date date,
+    category_id character varying(7),
+    subcategory_id character varying(10)
+);
+
+
+CREATE TABLE crowdfunding.category (
+    category_id character varying(7) NOT NULL,
+    category character varying(40)
+);
+
+
+CREATE TABLE crowdfunding.contacts (
+    contact_id integer NOT NULL,
+    first_name character varying(20),
+    last_name character varying(20),
+    email character varying(60)
+);
+
+
+CREATE TABLE crowdfunding.subcategory (
+    subcategory_id character varying(10) NOT NULL,
+    subcategory character varying(40)
+);
+
+
+ALTER TABLE ONLY crowdfunding.campaign
+    ADD CONSTRAINT pk_campaign PRIMARY KEY (cf_id);
+
+ALTER TABLE ONLY crowdfunding.category
+    ADD CONSTRAINT pk_category PRIMARY KEY (category_id);
+	
+ALTER TABLE ONLY crowdfunding.contacts
+    ADD CONSTRAINT pk_contacts PRIMARY KEY (contact_id);
+
+ALTER TABLE ONLY crowdfunding.subcategory
+    ADD CONSTRAINT pk_subcategory PRIMARY KEY (subcategory_id);
+
+
+ALTER TABLE ONLY crowdfunding.campaign
+    ADD CONSTRAINT fk_campaign_category_id FOREIGN KEY (category_id) REFERENCES crowdfunding.category(category_id);
+
+ALTER TABLE ONLY crowdfunding.campaign
+    ADD CONSTRAINT fk_campaign_contact_id FOREIGN KEY (contact_id) REFERENCES crowdfunding.contacts(contact_id);
+
+ALTER TABLE ONLY crowdfunding.campaign
+    ADD CONSTRAINT fk_campaign_subcategory_id FOREIGN KEY (subcategory_id) REFERENCES crowdfunding.subcategory(subcategory_id);
